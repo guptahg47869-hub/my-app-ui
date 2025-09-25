b2d52260cbc026752830eb6271b7133a39886205
guptahg47869-hub <gupta.hg47869@gmail.com>
2025-09-25 21:25:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from nicegui import ui # type: ignore
+from nicegui import ui
 
 # import all pages so they register with @ui.page
 import pages.trees
@@ -10,6 +10,8 @@ import pages.casting
 import pages.quenching
 # import pages.cutting
 import pages.reports
+
+app = ui.get_app()  # 👈 expose FastAPI app object
 
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run(title='Casting Tracker', reload=True)
