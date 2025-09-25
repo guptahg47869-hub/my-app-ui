c4173b658059644e10e2847280d2c5880f1d0855
guptahg47869-hub <gupta.hg47869@gmail.com>
2025-09-25 21:51:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from nicegui import ui
 
-# import your pages
 import pages.trees
 import pages.post_flask
 import pages.home
@@ -11,4 +10,4 @@ import pages.quenching
 import pages.reports
 
 if __name__ in {"__main__", "__mp_main__"}:
-    ui.run(title='Casting Tracker', reload=False)  # reload=False for production
+    ui.run(title='Casting Tracker', reload=False)
